cfbc7cf97df9a4e65b1b48fe36b354bf5bff25fd
bumjin <swift08201@gmail.com>
2022-06-29 10:38:17 +0900

    asd

--- react-app/src/Component/Content.js
+++ react-app/src/Component/Content.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
 class Content extends Component {
+
+
     render() {
       return (
         <article>
-          <h2>{this.props.title}</h2>
+          <h2>{this.props.title}dd</h2>
           {this.props.desc}
         </article>
       );

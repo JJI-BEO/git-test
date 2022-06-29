9e40a6ab9c44f4306cac8d0cb0a642c101c460da
bumjin <swift08201@gmail.com>
2022-06-29 11:01:02 +0900

    bb

--- react-app/src/Component/Content.js
+++ react-app/src/Component/Content.js
@@ -6,7 +6,7 @@ class Content extends Component {
     render() {
       return (
         <article>
-          <h2>{this.props.title}dd</h2>
+          <h2>{this.props.title}</h2>
           {this.props.desc}
         </article>
       );
